7279df42e1d04fa2a86be008757a0bc47b95c0a1
socialcore-primary <om@socialcore.in>
2019-05-18 17:56:17 +0530

    tested more than one cosumer

--- consumer/consumer0.py
+++ consumer/consumer0.py
@@ -38,4 +38,4 @@ def get_video_stream():
                b'Content-Type: image/jpg\r\n\r\n' + msg.value + b'\r\n\r\n')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=4000, debug=True)
